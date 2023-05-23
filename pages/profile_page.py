ee35cef950a31621d980e74d801fe74caef32346
turgor <maxac153@gmail.com>
2023-05-23 18:53:29 +0500

    add: test add avatar

--- pages/profile_page.py
+++ pages/profile_page.py
@@ -3,14 +3,14 @@ from locators.user_profile_locators import UserAvatarProfile
 import time
 
 class ProfilePage(BasePage):
-    def authorization(self, email, password):
+    def authorization(self, email, password, img):
         self.element_is_visible(UserAvatarProfile.INPUT_EMAIL).send_keys(email)
         self.element_is_visible(UserAvatarProfile.INPUT_PASSWORD).send_keys(password)
         self.element_is_visible(UserAvatarProfile.BUTTON_AUTHORIZATION).click()
         time.sleep(3)
         self.element_is_visible(UserAvatarProfile.USER_LOGO).click()
         self.element_is_visible(UserAvatarProfile.USER_PERSONAL_ACCOUNT).click()
-        self.element_is_visible(UserAvatarProfile.USER_ICON_INPUT).send_keys("/home/turgor/PycharmProjects/UsersTestSelemium/img/img_8Kb.png")
+        self.element_is_visible(UserAvatarProfile.USER_ICON_INPUT).send_keys(img)
         self.element_is_visible(UserAvatarProfile.USER_SAVE_ICON).click()
         time.sleep(3)
         result = self.element_is_visible(UserAvatarProfile.USER_ICON).get_attribute("src")
--- tests/test_4_add_avatar.py
+++ tests/test_4_add_avatar.py
@@ -2,13 +2,25 @@ import pytest
 from pages.profile_page import ProfilePage
 
 @pytest.mark.run(order=1)
-def test_user_authorization(driver):
+def test_user_add_avatar_less_150Kb(driver):
     email = 'verygood@mail.ru'
     password = '123'
+    img = "/home/turgor/PycharmProjects/UsersTestSelemium/img/img_8Kb.png"
 
     from_page = ProfilePage(driver, 'http://users.bugred.ru/user/login/index.html')
     from_page.open()
-    result = from_page.authorization(email, password)
+    result = from_page.authorization(email, password, img)
 
+    assert not 'http://users.bugred.ru/tmp/default_avatar.jpg' == result
 
-    assert  not 'http://users.bugred.ru/tmp/default_avatar.jpg' == result
+@pytest.mark.run(order=2)
+def test_user_add_avatar_more_150Kb(driver):
+    email = 'verygood@mail.ru'
+    password = '123'
+    img = "/home/turgor/PycharmProjects/UsersTestSelemium/img/img_more_150Kb.jpg"
+
+    from_page = ProfilePage(driver, 'http://users.bugred.ru/user/login/index.html')
+    from_page.open()
+    result = from_page.authorization(email, password, img)
+
+    assert 'http://users.bugred.ru/tmp/default_avatar.jpg' == result
